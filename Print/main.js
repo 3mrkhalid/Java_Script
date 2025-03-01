c7fa2caa3cd49a0c1cf5edc05acc3ebd769248e9
3mrkhalid <amrahmedyoussef100@gmail.com>
2025-03-02 01:37:38 +0200

    initial

---
+++ Print/main.js
@@ -0,0 +1,12 @@
+// Writes "hello world" directly into the HTML document
+document.write('hello world'); 
+
+//document => control html file
+
+// Logs "hello world" to the browser console
+console.log('hello world'); 
+
+// Displays an alert popup with "hello"
+window.alert('hello world'); 
+
+//window => control for anything in a browser
