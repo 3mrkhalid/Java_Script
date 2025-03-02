d7e9cdc30da7bad2155fa87767e2afd3c9d32277
3mrkhalid <amrahmedyoussef100@gmail.com>
2025-03-03 01:17:40 +0200

    Update

---
+++ 05) mathematical operations/main.js
@@ -0,0 +1,21 @@
+let num1 = 5;
+let num2 = 4;
+
+
+// Performing basic arithmetic operations in JavaScript
+
+console.log(num1 + num2); // Addition: Adds num1 and num2
+console.log(num1 - num2); // Subtraction: Subtracts num2 from num1
+console.log(num1 * num2); // Multiplication: Multiplies num1 by num2
+console.log(num1 / num2); // Division: Divides num1 by num2
+console.log(num1 * num2); // Multiplication
+console.log(num1 ** num2); // Exponentiation: Raises num1 to the power of num2
+//---------------------------------------------------------------------------------
+console.log('num' + 5);  // Output: "num5" (String concatenation)
+console.log('num' - 5);  // Output: NaN (Not a Number)
+
+//---------------------------------------------------------------------------------
+
+console.log(num1++); // Output: 5 (original value before increment) => num
+console.log(++num2); // Output: 7 (incremented value) => num +=1
+
